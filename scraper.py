70c005abc7f57300d5285b515f5293208bea2949
rushi-k-op <rushikaduskar1@gmail.com>
2026-02-13 13:21:35 +0100

    Update URL for status text in scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import sys
 
 # --- CONFIGURATION ---
 # PASTE YOUR GIST RAW URL HERE!
-URL = "https://gist.githubusercontent.com/rushi-k-op/0b9cda9f72887ad6431a3bb714d40ce0/raw/05718923d17c761d0082e85f3d0d7bf125e6b1ad/status.txt" 
+URL = "https://gist.githubusercontent.com/rushi-k-op/0b9cda9f72887ad6431a3bb714d40ce0/raw/1e9fd12c12753d5f8cdb4133c8206ed0c007f5ba/status.txt" 
 
 # The "Safety Phrase". If this exists, we assume NO apartments.
 TARGET_TEXT = "No results found for the given search criteria"
